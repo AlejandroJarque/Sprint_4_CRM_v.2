09027565a0a27fd686c6abbc3ad3049bbb386dd5
Jaki Riggs <jarquealex0302@gmail.com>
2025-12-24 10:34:45 +0100

    footer.blade added

---
+++ CRM_V2/resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="bg-stone-800 border-b border-gray-600 font-mono text-blue-400 dark:text-sky-400
+    h-16 flex items-center flex justify-center">
+        <p>NEXORA © {{ date('Y') }} - All rights reserved./</p>
+        <p>Version 1.0 - Project in development</p>
+    </div>
+</footer>
