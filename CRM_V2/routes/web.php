73263375ddc3640039518be71033bff2a8be21ee
Jaki Riggs <jarquealex0302@gmail.com>
2025-12-24 17:23:35 +0100

    Routes.web added

--- CRM_V2/routes/web.php
+++ CRM_V2/routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-=======
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
->>>>>>> feature/project-skeleton
