172add94dd905ddc8dc489fa3c97c171c7eb4dee
Jaki Riggs <jarquealex0302@gmail.com>
2025-12-24 18:34:30 +0100

    routes added

--- CRM_V2/routes/web.php
+++ CRM_V2/routes/web.php
@@ -19,3 +19,16 @@ Route::get('/register', [\App\Http\Controllers\UserController::class, 'registerF
 
 Route::post('/register', [\App\Http\Controllers\UserController::class, 'registerAction'])
 ->middleware('guest');
+
+
+Route::prefix('crm')->middleware('auth')->group(function () {
+        Route::get('/profile', [\App\Http\Controllers\UserController::class, 'profileAction'])
+    ->name('profile');
+
+    Route::put('/profile', [\App\Http\Controllers\UserController::class, 'updateProfileAction'])
+    ->name('profile.update');
+
+    Route::post('/logout', [\App\Http\Controllers\UserController::class, 'logoutAction'])
+    ->name('logout');
+
+});
